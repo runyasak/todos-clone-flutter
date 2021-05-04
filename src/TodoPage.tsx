ed1a84acd38b27038177704e08e7922a500f4bf0
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-05-04 18:38:11 +0700

    feat: implement conver image with card styled

--- src/TodoPage.tsx
+++ src/TodoPage.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { useFela } from 'react-fela';
 
 const TodoPage: React.FC = () => {
-  return <h1>Hello Todo!</h1>;
+  const { css } = useFela();
+
+  return <div className={css({ backgroundColor: '#B296AC' })}>
+    <div className={css({
+      maxWidth: '414px',
+      margin: 'auto',
+    })}>
+      <img
+        className={css({
+          width: '100%',
+          objectFit: 'cover',
+        })}
+        src="/cover.png"
+        alt="Cover Todo"
+      />
+      <div className={css({
+        borderTopLeftRadius: '32px',
+        borderTopRightRadius: '32px',
+        padding: '24px',
+        backgroundColor: 'white',
+        position: 'relative',
+        marginTop: '-32px',
+        height: 'calc(100vh - 433px)',
+      })}>
+        <b className={css({ fontSize: '24px' })}>Todo</b>
+      </div>
+    </div>
+  </div>;
 };
 
 export default TodoPage;
