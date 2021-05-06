0b354b941e9840c99d13c9d0fb6a6258ff229ff7
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-05-06 23:42:16 +0700

    feat: add todos prop and update complete icon

--- src/components/TodoListScreen.tsx
+++ src/components/TodoListScreen.tsx
@@ -1,33 +1,56 @@
 import React from 'react';
 import { useFela } from 'react-fela';
 import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
-import { purple } from '@material-ui/core/colors';
+import { green, purple } from '@material-ui/core/colors';
+import { CheckCircleOutlineRounded } from '@material-ui/icons';
 import PanoramaFishEyeIcon from '@material-ui/icons/PanoramaFishEye';
 import TitleBar from './TitleBar';
+import type { Todo } from '../models/Todo';
 
 export interface TodoListScreenProps {
+  todos?: Todo[],
   onClickNew?: () => void;
 }
 
 const TodoListScreen: React.FC<TodoListScreenProps> = ({
+  todos = [],
   onClickNew,
 }) => {
   const { css } = useFela();
 
+  const renderCompleteIcon = (complete: boolean) => complete
+    ? <CheckCircleOutlineRounded
+        className={css({
+          color: green[500],
+          cursor: 'pointer'
+        })}
+      />
+    : <PanoramaFishEyeIcon
+        className={css({
+          color: purple[200],
+          cursor: 'pointer'
+        })}
+      />
+
+  const renderListItem = todos.map((todo, index) => (
+    <ListItem
+      key={index}
+      alignItems="flex-start"
+    >
+      <ListItemIcon>
+        {renderCompleteIcon}
+      </ListItemIcon>
+      <ListItemText
+        primary={todo.topic}
+        secondary={todo.msg}
+      />
+    </ListItem>
+  ));
+
   return <>
     <TitleBar actionName="new" action={onClickNew}></TitleBar>
     <List>
-      <ListItem alignItems="flex-start">
-        <ListItemIcon>
-          <PanoramaFishEyeIcon
-            className={css({ color: purple[200], cursor: 'pointer' })}
-          />
-        </ListItemIcon>
-        <ListItemText
-          primary="อ่านหนังสือหลังตื่นนอน"
-          secondary="อ่านเล่ม ทำยังไงให้สาธิดีขึ้น"
-        />
-      </ListItem>
+      {renderListItem}
     </List>
   </>
 }
